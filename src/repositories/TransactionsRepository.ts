b177bebf6fa0a52a950e6a7438793bb220325678
charleseduardome <charleseduardo.me@gmail.com>
2020-04-15 15:35:11 -0300

    Challenge finished

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,5 +1,11 @@
 import Transaction from '../models/Transaction';
 
+interface CreateTransaction {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 interface Balance {
   income: number;
   outcome: number;
@@ -14,15 +20,39 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const incomeSum = this.transactions.reduce((total, transaction) => {
+      const { value } = transaction;
+
+      return transaction.type === 'income' ? total + value : total;
+    }, 0);
+
+    const outcomeSum = this.transactions.reduce((total, transaction) => {
+      const { value } = transaction;
+
+      return transaction.type === 'outcome' ? total + value : total;
+    }, 0);
+
+    const total = incomeSum - outcomeSum;
+
+    const balance: Balance = {
+      income: incomeSum,
+      outcome: outcomeSum,
+      total,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransaction): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
